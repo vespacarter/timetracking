1e03f68a0e01403ba57431d5d137a61457aecacc
vespacarter <skasez@gmail.com>
2015-12-05 13:27:28 +0100

    request with parameters

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,25 @@
 class ProjectsController < ApplicationController
 	def index
 		#render index by default! 
-		@projects = Project.last_created_projects(5)
+		#@projects = Project.last_created_projects(5)
+		@projects = Project.all
 	end
+
+	def show
+		@project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			render 'no_projects_found'
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		p = Project.new(name: params[:project][:name], description: params[:project][:description])
+		p.save
+
+		redirect_to '/projects'
+	end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
   get '/projects' => 'projects#index'
+  get '/projects/new' => 'projects#new'
+  get '/projects/:id' => "projects#show"
+  post '/projects' => 'projects#create'
 end
